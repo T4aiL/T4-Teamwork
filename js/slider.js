0eb7a0a4bed63b2ee0c285eefe5711ef204ac890
Myszexx <michalr550@gmail.com>
2021-11-29 06:53:24 +0100

    slider 2

--- js/slider.js
+++ js/slider.js
@@ -9,12 +9,23 @@ class Post {
 
 const slider = document.getElementById("slider")
 
-let post1 = new Post ("post1", "lorem pusm", "michal")
-let post2 = new Post ("post2", "lorem pusm", "michal")
-let post3 = new Post ("post3", "lorem pusm", "michal")
+let post1 = new Post ("post1", "lorem pusm", "michal", '/img/back.png')
+let post2 = new Post ("post2", "lorem pusm", "michal", '/img/back.png')
+let post3 = new Post ("post3", "lorem pusm", "michal", '/img/back.png')
 
 
 
 
 let post = [post1, post2, post3]
 
+function slider() {
+
+}
+
+var slideIndex = 0;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
